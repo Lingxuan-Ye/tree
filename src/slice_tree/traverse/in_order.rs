35cbc730d7178418b15d8cffec26ebc55ec7970e
Lingxuan Ye <love@lingxuan.io>
2026-01-04 06:45:28 +0800

    refactor: rewrite slice tree traversal logic

--- src/slice_tree/traverse/in_order.rs
+++ src/slice_tree/traverse/in_order.rs
@@ -1,25 +1,25 @@
-use crate::{CompleteTree, Index};
+use crate::index::Index;
 use alloc::vec::Vec;
 use core::iter::FusedIterator;
 use core::marker::PhantomData;
 
 #[derive(Debug, Clone)]
 pub struct InOrder<'a, T> {
-    state: State,
-    stack: Vec<Index<2>>,
-    tree: &'a [T],
+    indices: InOrderIndices,
+    root: *const T,
+    marker: PhantomData<&'a T>,
 }
 
 impl<'a, T> InOrder<'a, T> {
     pub fn new(tree: &'a [T]) -> Self {
-        let state = if tree.is_empty() {
-            State::Done
-        } else {
-            State::Push(Index::root())
-        };
-        let capacity = CompleteTree::<2>::height(tree);
-        let stack = Vec::with_capacity(capacity);
-        Self { state, stack, tree }
+        let indices = InOrderIndices::new(tree.len());
+        let root = tree.as_ptr();
+        let marker = PhantomData;
+        Self {
+            indices,
+            root,
+            marker,
+        }
     }
 }
 
@@ -27,44 +27,13 @@ impl<'a, T> Iterator for InOrder<'a, T> {
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            match self.state {
-                State::Push(index) => {
-                    if let Some(left_child) = index.left_child()
-                        && left_child.to_flattened() < self.tree.len()
-                    {
-                        self.state = State::Push(left_child);
-                        self.stack.push(index);
-                        continue;
-                    }
-                    self.state = State::Pop;
-                    let index = index.to_flattened();
-                    return Some(unsafe { self.tree.get_unchecked(index) });
-                }
-
-                State::Pop => {
-                    if let Some(index) = self.stack.pop() {
-                        if let Some(right_child) = index.right_child()
-                            && right_child.to_flattened() < self.tree.len()
-                        {
-                            self.state = State::Push(right_child);
-                        }
-                        let index = index.to_flattened();
-                        return Some(unsafe { self.tree.get_unchecked(index) });
-                    }
-                    self.state = State::Done;
-                    return None;
-                }
-
-                State::Done => {
-                    return None;
-                }
-            }
-        }
+        let index = self.indices.next()?;
+        let node = unsafe { &*self.root.add(index) };
+        Some(node)
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.stack.len(), Some(self.tree.len()))
+        self.indices.size_hint()
     }
 }
 
@@ -72,27 +41,19 @@ impl<T> FusedIterator for InOrder<'_, T> {}
 
 #[derive(Debug)]
 pub struct InOrderMut<'a, T> {
-    state: State,
-    stack: Vec<Index<2>>,
-    tree: *mut [T],
+    indices: InOrderIndices,
+    root: *mut T,
     marker: PhantomData<&'a mut T>,
 }
 
 impl<'a, T> InOrderMut<'a, T> {
     pub fn new(tree: &'a mut [T]) -> Self {
-        let state = if tree.is_empty() {
-            State::Done
-        } else {
-            State::Push(Index::root())
-        };
-        let capacity = CompleteTree::<2>::height(tree);
-        let stack = Vec::with_capacity(capacity);
-        let tree = tree as *mut [T];
+        let indices = InOrderIndices::new(tree.len());
+        let root = tree.as_mut_ptr();
         let marker = PhantomData;
         Self {
-            state,
-            stack,
-            tree,
+            indices,
+            root,
             marker,
         }
     }
@@ -102,30 +63,78 @@ impl<'a, T> Iterator for InOrderMut<'a, T> {
     type Item = &'a mut T;
 
     fn next(&mut self) -> Option<Self::Item> {
+        let index = self.indices.next()?;
+        let node = unsafe { &mut *self.root.add(index) };
+        Some(node)
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        self.indices.size_hint()
+    }
+}
+
+impl<T> FusedIterator for InOrderMut<'_, T> {}
+
+#[derive(Debug, Clone)]
+struct InOrderIndices {
+    state: State,
+    stack: Vec<usize>,
+    tree_len: usize,
+}
+
+impl InOrderIndices {
+    fn new(tree_len: usize) -> Self {
+        if tree_len == 0 {
+            let state = State::Done;
+            let stack = Vec::new();
+            return Self {
+                state,
+                stack,
+                tree_len,
+            };
+        }
+        let index = const { Index::<2>::root().to_flattened() };
+        let state = State::Push(index);
+        let last = tree_len - 1;
+        let tree_height = Index::<2>::from_flattened(last).depth();
+        let capacity = tree_height;
+        let stack = Vec::with_capacity(capacity);
+        Self {
+            state,
+            stack,
+            tree_len,
+        }
+    }
+}
+
+impl Iterator for InOrderIndices {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
         loop {
             match self.state {
                 State::Push(index) => {
-                    if let Some(left_child) = index.left_child()
-                        && left_child.to_flattened() < self.tree.len()
-                    {
-                        self.state = State::Push(left_child);
-                        self.stack.push(index);
-                        continue;
+                    if let Some(left_child) = Index::from_flattened(index).left_child() {
+                        let left_child = left_child.to_flattened();
+                        if left_child < self.tree_len {
+                            self.state = State::Push(left_child);
+                            self.stack.push(index);
+                            continue;
+                        }
                     }
                     self.state = State::Pop;
-                    let index = index.to_flattened();
-                    return Some(unsafe { (&mut *self.tree).get_unchecked_mut(index) });
+                    return Some(index);
                 }
 
                 State::Pop => {
                     if let Some(index) = self.stack.pop() {
-                        if let Some(right_child) = index.right_child()
-                            && right_child.to_flattened() < self.tree.len()
-                        {
-                            self.state = State::Push(right_child);
+                        if let Some(right_child) = Index::from_flattened(index).right_child() {
+                            let right_child = right_child.to_flattened();
+                            if right_child < self.tree_len {
+                                self.state = State::Push(right_child);
+                            }
                         }
-                        let index = index.to_flattened();
-                        return Some(unsafe { (&mut *self.tree).get_unchecked_mut(index) });
+                        return Some(index);
                     }
                     self.state = State::Done;
                     return None;
@@ -139,15 +148,15 @@ impl<'a, T> Iterator for InOrderMut<'a, T> {
     }
 
     fn size_hint(&self) -> (usize, Option<usize>) {
-        (self.stack.len(), Some(self.tree.len()))
+        (self.stack.len(), Some(self.tree_len))
     }
 }
 
-impl<T> FusedIterator for InOrderMut<'_, T> {}
+impl FusedIterator for InOrderIndices {}
 
 #[derive(Debug, Clone)]
 enum State {
-    Push(Index<2>),
+    Push(usize),
     Pop,
     Done,
 }
